18a55b4e66cfdb3f1d8634e83fbec8adbcd4e83d
Quilty <concentric12@hotmail.com>
2017-11-06 11:41:44 -0800

    (refactor) prep server for multiplier calculator file

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const sqs = require('./sqs');
 setInterval(sqs.getEyeballs, 10000);
 setInterval(sqs.getDrivers, 10000);
 setInterval(sqs.getBookings, 10000);
-setInterval(sqs.sendMultipliers, 10000);
+setInterval(() => {
+  const multipliers = calculateMultipliers();
+  sqs.sendMultipliers(multipliers);
+}, 10000);
 
 app.listen(port, () => console.log(`listening on ${port}`));
